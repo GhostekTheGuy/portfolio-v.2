f095aab33dbee0b4ebea83d531c72e0fad49682c
Hubert Kolejko <83242010+GhostekTheGuy@users.noreply.github.com>
2024-03-05 17:12:35 +0100

    gg

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -1,16 +1,17 @@
 "use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavLink = ({ link }) => {
     const pathName = usePathname();
     console.log(pathName);
-    
+
     return (
-      <Link className="" href={link.url}>
-        {link.title}
-      </Link>
+        <a className={`rounded p-1 ${pathName===link.url ? "bg-black text-white" : ""}`} href={link.url}>
+            {link.name}
+        </a>
     );
-  };
-  
-  export default NavLink;
+};
+
+export default NavLink;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,8 @@
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
-import navLink from "./navLink";
+import NavLink from "./navLink";
+import {motion} from "framer-motion"
 
 const links =[
     {url: '/', name: 'Home'},
@@ -13,17 +14,71 @@ const links =[
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+
+    const topVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:45,
+            backgroundColor:"rgb(255, 255, 255)"
+        },
+    }
+
+    const centerVariants={
+        closed:{
+            opacity:1,
+        },
+        opened:{
+            opacity:0,
+        },
+    }
+
+    const bottomVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:-45,
+            backgroundColor:"rgb(255, 255, 255)"
+        },
+    }
+
+    const listVariants={
+        closed:{
+            x:"100vh",
+        },
+        opened:{
+            x:0,
+            transition:{
+                when:"beforeChildren",
+                staggerChildren:0.2,
+            },
+        },
+    };
+
+    const listItemVariants={
+        closed:{
+            x:-10,
+            opacity:0,
+        },
+        opened:{
+            x:0,
+            opacity:1,
+        },
+    };
+
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
             {/*LINKS*/}
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map(link =>(
-                    <Link href={link.url} key={link.name}>{link.name}</Link>
+                    <NavLink link={link} key={link.name}></NavLink>
                 ))}
             </div>
             {/*LOGO*/}
-            <div className="flex md:hidden lg:flex w-1/3 justify-center">
-                <Link href="/" 
+            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+                <Link href="/"
                 className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white mr-1">ghostek</span>
                     <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.media</span>
@@ -31,6 +86,8 @@ const Navbar = () => {
             </div>
             {/*SOCIAL*/}
             <div className="hidden md:flex gap-4 w-1/3">
+                    <Image src="/pinterest.png" alt="" width={24} height={24} />
+                    <Image src="/pinterest.png" alt="" width={24} height={24} />
                 <Link href="https://github.com/GhostekTheGuy">
                     <Image src="/github.png" alt="" width={24} height={24} />
                 </Link>
@@ -47,23 +104,35 @@ const Navbar = () => {
             {/*MENU*/}
             <div className="md:hidden">
                 {/*MENU BUTTON*/}
-                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={()=>setOpen((prev) => !prev)}>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" 
+                onClick={()=>setOpen((prev) => !prev)}>
+                    <motion.div variants={topVariants}
+                    animate={open ? "opened" : "closed"}
+                    className="w-10 h-1 bg-black rounded origin-left">
+                    </motion.div>
+                    <motion.div variants={centerVariants}
+                    animate={open ? "opened" : "closed"}
+                    className="w-10 h-1 bg-black rounded">
+                    </motion.div>
+                    <motion.div variants={bottomVariants}
+                    animate={open ? "opened" : "closed"}
+                    className="w-10 h-1 bg-black rounded origin-left">
+                    </motion.div>
                 </button>
                 {/*MENU LIST*/}
                 {open && (
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                <motion.div variants={listVariants} initial="closed" animate="opened" 
+                className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
                     {links.map((link)=>(
-                        <Link href={link.url} key={link.name}>
+                        <motion.div variants={listItemVariants} className="" key={link.name}>
+                        <Link href={link.url}>
                             {link.name}
                         </Link>
-                    ))} 
-                </div>
+                        </motion.div>
+                    ))}
+                </motion.div>
             )}
             </div>
-            
         </div>
     );
 };
